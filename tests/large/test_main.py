6bca345a7759c6bb943804f7f4f6b81367ee36ed
tktkc72 <32025300+tktkc72@users.noreply.github.com>
2020-01-18 16:52:22 +0900

    テスト治安向上

--- tests/large/test_main.py
+++ tests/large/test_main.py
@@ -1,7 +1,11 @@
 from main import main
+import pytest
 
 
+@pytest.mark.large
 def test_main(mocker):
+    """depending on Google Keep"""
+    # need to set the environ "KEEP_PASSWORD"
     request = mocker.Mock()
     request.get_json.return_value = {"item": "肉"}
 
