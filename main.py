2d1e5860bf657026e298f299e27900e234fd9a25
tkc <tkc@MacBook-Pro.local>
2020-02-09 11:00:07 +0900

    keep操作部分を分割

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
-import os
-from typing import Any, Generator
+from flask import Request
 
-from gkeepapi import Keep
-from gkeepapi.node import List as Glist
-from gkeepapi.node import NewListItemPlacementValue
-
-from lib.configs import ConfigParser
+from gkeeper.configs import ConfigParser
+from gkeeper.keeper import Keeper
 
 
-def main(request: Any) -> None:
+def main(request: Request) -> None:
     request_json = request.get_json()
     if 'item' not in request_json:
         raise ValueError('request body doesnt include the key "item"')
     item = request_json['item']
 
     config_parser = ConfigParser()
-    google_config = config_parser.google_config
-    keep_config = config_parser.keep_config
 
-    keep = Keep()
-    _login(keep, google_config.id)
-    glist = _fetch_list(keep, keep_config.list_name)
-    glist.add(item, False,
-              NewListItemPlacementValue.Bottom)
-    keep.sync()
-
-
-def _login(keep: Keep, user: str) -> None:
-    password = os.environ.get('KEEP_PASSWORD', 'password')
-    result = keep.login(user, password)
-    if not result:
-        raise ValueError('cant login')
-
-
-def _fetch_list(keep: Keep, title: str) -> Glist:
-    glists: Generator[Glist, None, None] = keep.find(query=title)
-    glist = next(glists)
-    return glist
+    keeper = Keeper(config_parser=config_parser)
+    keeper.add(item=item)
